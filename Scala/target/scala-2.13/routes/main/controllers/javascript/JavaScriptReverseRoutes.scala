e1fc428d7593878a4290351330e5a29ba22305f7
aka takatejr aka person <perkadamian@gmail.com>
2020-09-27 00:52:05 +0200

    reload

---
+++ Scala/target/scala-2.13/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,54 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:D:/Dailis/Scala/conf/routes
+// @DATE:Sat Sep 26 17:49:39 CEST 2020
+
+import play.api.routing.JavaScriptReverseRoute
+
+
+import _root_.controllers.Assets.Asset
+
+// @LINE:7
+package controllers.javascript {
+
+  // @LINE:7
+  class ReverseHomeController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:7
+    def index: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.HomeController.index",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + """"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:10
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:10
+    def versioned: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.versioned",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[play.api.mvc.PathBindable[Asset]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+
+}
