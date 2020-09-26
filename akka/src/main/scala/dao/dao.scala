35a505cfd29926d2d02e009cee74d3c2876cb653
aka takatejr aka person <perkadamian@gmail.com>
2020-09-27 00:46:53 +0200

    reload scala play to scala akka

---
+++ akka/src/main/scala/dao/dao.scala
@@ -0,0 +1,15 @@
+trait Dao[T, U[_]] {
+  def delete(id: Int) : U[Int]
+
+  def update(id: Int, row: T): U[Int]
+
+  def createSchema(): U[Unit]
+
+  def insert(row: T): U[Int]
+
+  def selectAll(page: Int, pageSize: Int, sort: String): U[Seq[T]]
+
+  def select(id: Int): U[Option[T]]
+
+  def sortingFields: Set[String]
+}
